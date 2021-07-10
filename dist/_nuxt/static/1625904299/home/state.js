0b8f8a429bb2e0d6b3318da8804e7d2dd8cdca71
Gerald Heng <geralddevelop@gmail.com>
2021-07-10 16:10:28 +0800

    adding dist subtree

--- dist/_nuxt/static/1625904299/home/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdaisy-web\u002F_nuxt\u002Fstatic\u002F1625904299",layout:"default",error:a,serverRendered:true,routePath:"\u002Fhome",config:{_app:{basePath:"\u002Fdaisy-web\u002F",assetsPath:"\u002Fdaisy-web\u002F_nuxt\u002F",cdnURL:a}}}}(null));
